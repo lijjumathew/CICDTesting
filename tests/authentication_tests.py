3daf74cf39dc715e6c497148fbe78b63f2c48a5e
lijjumathew <lijju.mathew@yahoo.com>
2017-02-13 16:30:00 -0600

    Initial commit

---
+++ tests/authentication_tests.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from mock import patch
+import cicd_testing.authentication as auth
+
+class StandAloneTests(TestCase):
+    """Test the stand-alone module functions."""
+
+    @patch('__builtin__.open')
+    def test_login(self,mock_open):
+        """Test the login function."""
+        mock_open.return_value.read.return_value = "george|bosco\n"
+        self.assertTrue(auth.login('george', 'bosco'))
+
