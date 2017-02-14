936a1a698031c28c273ca7e92c0092907b2be6ca
lijjumathew <lijju.mathew@yahoo.com>
2017-02-14 10:56:21 -0600

    added few more tests

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -11,3 +11,15 @@ class StandAloneTests(TestCase):
         """Test the login function."""
         mock_open.return_value.read.return_value = "lijju|mathew\n"
         self.assertTrue(auth.login('lijju', 'mathew'))
+
+    @patch('builtins.open')
+    def test_login_bad_creds(self, mock_open):
+        """Test the login function when bad creds are passed."""
+        mock_open.return_value.read.return_value = "lijju|mathew\n"
+        self.assertFalse(auth.login('lijju', 'wrongpassword'))
+
+    @patch('builtins.open')
+    def test_login_error(self, mock_open):
+        """Test the login function when an error happens."""
+        mock_open.side_effect = IOError()
+        self.assertFalse(auth.login('lijju', 'mathew'))
